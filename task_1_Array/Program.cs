a5eda1e2e80642bc9ecf958ec951afd836c7f8f3
RitaBozhko <79147042+RitaBozhko@users.noreply.github.com>
2022-08-17 11:32:48 +0300

    Добавление программы с решением

---
+++ task_1_Array/Program.cs
@@ -0,0 +1,23 @@
+﻿string[] arrayOriginal = {"hello", "2", "world", ":-)"};
+string[] arrayNew = new string[arrayOriginal.Length];
+ 
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+PrintArray(arrayOriginal);
+
+for (int i = 0; i < arrayOriginal.Length; i++)
+{
+    if(arrayOriginal[i].Length <= 3)
+    {
+        arrayNew[i] = arrayOriginal[i];
+    }
+}
+
+Console.WriteLine();
+PrintArray(arrayNew);
